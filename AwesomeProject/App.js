d3788055eabc89f610d5dd65986f7877dca3b7fb
RomainConte <146077031+RomainConte@users.noreply.github.com>
2024-05-17 09:33:22 +0200

    [PUT] import overlay on Home.js

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,15 +1,21 @@
+import { AppRegistry } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { Home, Portfolio, Setting, Start, Start2 } from './screen'; // Assurez-vous que Start2 est importé
+import { Home, Portfolio, Setting, Start, Start2 } from './screen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
 import { MaterialIcons } from '@expo/vector-icons';
 import { Entypo } from '@expo/vector-icons';
 import { Feather } from '@expo/vector-icons';
+import CustomDrawerContent from './component/CustomDrawerContent.js';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import 'react-native-gesture-handler';
+import { name as appName } from './app.json';
 
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
 
 function MyTabs() {
   return (
@@ -48,14 +54,31 @@ function MyTabs() {
   );
 }
 
+function MainNavigator() {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Start" component={Start} />
+      <Stack.Screen name="Start2" component={Start2} />
+      <Stack.Screen name="Main" component={DrawerNavigator} />
+    </Stack.Navigator>
+  );
+}
+
+function DrawerNavigator() {
+  return (
+    <Drawer.Navigator
+      drawerContent={(props) => <CustomDrawerContent {...props} />}
+      screenOptions={{ headerShown: false }}
+    >
+      <Drawer.Screen name="Home" component={MyTabs} />
+    </Drawer.Navigator>
+  );
+}
+
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Start" component={Start} />
-        <Stack.Screen name="Start2" component={Start2} /> 
-        <Stack.Screen name="Main" component={MyTabs} />
-      </Stack.Navigator>
+      <MainNavigator />
     </NavigationContainer>
   );
 }
@@ -71,3 +94,5 @@ const screenoption = {
     fontWeight: 'bold',
   },
 };
+
+AppRegistry.registerComponent(appName, () => App);
